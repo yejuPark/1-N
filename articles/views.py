ae260239e71fede185fde66bafa16f8105680a66
yejuPark <pyj8977@daum.net>
2023-08-25 11:07:19 +0900

    Read, Create

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
+
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'index.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+
+
+    else:
+        form = ArticleForm()
+
+    context = {
+        'form': form,
+    }
+    
+    return render(request, 'form.html', context)
